1fbdf197f441aa40206ad8c21620eef4768827fb
Allyson Washburn <allyson680@gmail.com>
2025-09-25 09:36:33 -0700

    chore: skip lint and TS errors during production build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Skip lint and type errors in Vercel builds
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
